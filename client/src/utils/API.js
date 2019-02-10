98baa0c5222faf40981894c9d5fac6f270d0d9e1
Jowensky Neard <jp.12@live.com>
2019-02-09 22:47:20 -0500

    Proxy

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,19 +3,19 @@ import axios from "axios";
 export default {
   // Gets all books
   getBooks: function() {
-    return axios.get(`http://localhost:3001/api/book`);
+    return axios.get(`/api/book`);
   },
   // Gets the book with the given id
   getBookId: function(id) {
-    return axios.get(`http://localhost:3001/api/book/${id}`);
+    return axios.get(`api/book/${id}`);
   },
   // Deletes the book with the given id
   deleteBook: function(bookId) {
-    return axios.delete(`http://localhost:3001/api/book/${bookId}`);
+    return axios.delete(`/api/book/${bookId}`);
   },
   // Saves a book to the database
   saveBook: function(bookData) {
-    return axios.post(`http://localhost:3001/api/book`, bookData);
+    return axios.post(`/api/book`, bookData);
   },
   googleBooks: function(query) {
     return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + query)
